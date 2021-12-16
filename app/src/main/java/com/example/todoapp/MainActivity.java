9cd48a77800912ef6fe5f8112027f7e5d6fb4600
Binay Shah <binayshah@Binays-MacBook-Pro.local>
2021-12-16 08:10:41 +0545

    implemented repository and todo model

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Repository repository;
+    List<Todo> todos;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
